1f9bd45ecea33b826ba88726c2f550b2fdfdddd1
GitHub-Sanjit <sanjit.est14@gamil.com>
2024-04-23 20:15:35 +0600

    Http Methods

--- server/index.js
+++ server/index.js
@@ -1,10 +1,32 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
-  const log = `${Date.now()} ${req.url}: New Req Received\n`;
+  if (req.url === "/favicon.ico") return res.end();
+  const log = `${Date.now()} ${req.method} ${req.url}: New Req Received\n`;
+  const myUrl = url.parse(req.url,true)
+  console.log(myUrl)
   fs.appendFile("log.txt", log, (err, data) => {
-    res.end("Hello From Server");
+    switch (myUrl.pathname) {
+      case "/":
+        res.end("HOME PAGE.")
+        break;
+      case "/about":
+        res.end("I am Sanjit Sarkar")
+        break;
+      case "/signup":
+        if (req.method === "GET") res.end("This is a signup Form.")
+        else if (req.method == "POST") {
+          // DB query
+          res.end("Success");
+        }
+        break;
+    
+      default:
+        res.end("404 Page not Found")
+        break;
+    }
   });
 });
 
