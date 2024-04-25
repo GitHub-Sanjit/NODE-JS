a0fa616860e107e248fd816e9cc85d8e4066cf23
GitHub-Sanjit <sanjit.est14@gamil.com>
2024-04-26 00:01:27 +0600

    Server Side Rendering

--- short-url-node/index.js
+++ short-url-node/index.js
@@ -1,20 +1,27 @@
 const express = require("express");
+const path = require("path");
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
 const URL = require("./models/url");
 
 const app = express();
-const PORT = 8000;
+const PORT = 8001;
 
 connectToMongoDB(
-  "mongodb+srv://node1:node1@cluster0.wz775d8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  "mongodb+srv://node1:node1@cluster0.wz775d8.mongodb.net/short-url?retryWrites=true&w=majority&appName=Cluster0"
 ).then(() => console.log("Mongodb connected"));
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/url", urlRoute);
+app.use("/", staticRoute);
 
-app.get("/:shortId", async (req, res) => {
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
