8902a8af7d51c41c309e38dd87945b9d3397bd3e
GitHub-Sanjit <sanjit.est14@gamil.com>
2024-04-26 15:39:10 +0600

    Authentication in Node

--- short-url-node/controllers/url.js
+++ short-url-node/controllers/url.js
@@ -10,7 +10,9 @@ async function handleGenerateNewShortURL(req, res) {
     shortId: shortID,
     redirectURL: body.url,
     visitHistory: [],
+    createdBy: req.user._id,
   });
+
   return res.render("home", {
     id: shortID,
   });
